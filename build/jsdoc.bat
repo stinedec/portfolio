fadb5c52cdb7b9007e5229e8c134418ca8b7dc36
George Pantazis <gpantazis@cpbgroup.com>
2012-10-10 13:01:46 -0600

    Eject old build scripts from boilerplate. Promote Grunt to the front of the line.

--- build/jsdoc.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set JS_BASE_DIR=tools\jsdoc\jsdoc-toolkit
-java -jar %JS_BASE_DIR%\jsrun.jar %JS_BASE_DIR%\app\run.js -c=jsdoc.conf
--- build/jsdoc.sh
+++
@@ -1,1 +0,0 @@
-java -jar tools/jsdoc/jsdoc-toolkit/jsrun.jar tools/jsdoc/jsdoc-toolkit/app/run.js -c=jsdoc.conf
