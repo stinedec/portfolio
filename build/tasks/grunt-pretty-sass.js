98207fb3849810684d7b229078af660304f67081
bminch <bminch@cpbgroup.com>
2013-01-30 11:24:31 -0700

    pretty-sass now with tabs and --in-place conversion.

--- build/tasks/grunt-pretty-sass.js
+++ build/tasks/grunt-pretty-sass.js
@@ -2,40 +2,44 @@
  * grunt-pretty-sass
  * http://gruntjs.com/
  *
- * Copyright (c) 2013 Brandon Minch, contributors
+ * Copyright (c) 2013 Brandon Minch
  * Licensed under the MIT license.
  */
 
 module.exports = function(grunt) {
 
-  'use strict';
+	'use strict';
 
-  var _ = grunt.utils._,
-    fs = require('fs'),
-    exec = require('child_process').exec;
+	var _ = grunt.utils._,
+		fs = require('fs'),
+		exec = require('child_process').exec;
 
-  grunt.registerMultiTask('pretty-sass', 'Format SASS source files', function() {
-    var files = grunt.file.expandFiles(this.file.src),
-      done = this.async();
+	grunt.registerMultiTask('pretty-sass', 'Format SASS source files', function() {
+		var files = grunt.file.expandFiles(this.file.src),
+			done = this.async(),
+			filesComplete = 0;
 
-    _.each(files, function(filepath, i) {
-      var dirtySass = grunt.file.read(filepath),
-          command = 'echo \'' + dirtySass + '\' | sass-convert --from scss --to scss --stdin';
+		_.each(files, function(filepath, i) {
+			var dirtySass = grunt.file.read(filepath),
+					command = 'sass-convert --from scss --to scss --indent t --in-place ' + filepath,
+					cyan = '\u001b[36m',
+					reset = '\u001b[0m';
 
-      grunt.log.writeln('prettifying: ' + filepath);
+			grunt.log.writeln(cyan + 'prettifying: ' + reset + filepath);
 
-      exec(command, function ( error, stdout, stderr ) {
-        if ( error !== null ) {
-          grunt.log.error( error );
-          done( false );
-        } else {
-          grunt.file.write(filepath, stdout);
-          if (i === files.length - 1) {
-            done( true );
-          }
-        }
-      });
+			exec(command, function ( error, stdout, stderr ) {
+				if ( error !== null ) {
+					grunt.log.error( filepath + ': ' + error );
+					done( false );
+				} else {
+					if (filesComplete === files.length - 1) {
+						done( true );
+					} else {
+						filesComplete++;
+					}
+				}
+			});
 
-    });
-  });
+		});
+	});
 };
