e2e5c45046a1b04ddd2ad53c352e8c3aec7c5ef4
bminch <bminch@cpbgroup.com>
2013-01-31 16:26:00 -0700

    initial alphabetization of scss files

--- build/tasks/grunt-pretty-sass.js
+++ build/tasks/grunt-pretty-sass.js
@@ -12,18 +12,21 @@ module.exports = function(grunt) {
 
 	var _ = grunt.utils._,
 		fs = require('fs'),
-		exec = require('child_process').exec;
+		exec = require('child_process').exec,
+		filesLength = 0,
+		filesComplete = 0,
+		cyan = '\u001b[36m',
+		reset = '\u001b[0m';
 
 	grunt.registerMultiTask('pretty-sass', 'Format SASS source files', function() {
 		var files = grunt.file.expandFiles(this.file.src),
-			done = this.async(),
-			filesComplete = 0;
+			done = this.async();
+
+		filesLength = files.length;
 
 		_.each(files, function(filepath, i) {
 			var dirtySass = grunt.file.read(filepath),
-					command = 'sass-convert --from scss --to scss --indent t --in-place ' + filepath,
-					cyan = '\u001b[36m',
-					reset = '\u001b[0m';
+					command = 'sass-convert --from scss --to scss --indent t --in-place ' + filepath;
 
 			grunt.log.writeln(cyan + 'prettifying: ' + reset + filepath);
 
@@ -32,14 +35,93 @@ module.exports = function(grunt) {
 					grunt.log.error( filepath + ': ' + error );
 					done( false );
 				} else {
-					if (filesComplete === files.length - 1) {
-						done( true );
-					} else {
-						filesComplete++;
-					}
+
+				alphaebetize(filepath);
+
 				}
 			});
-
 		});
+
+		function alphaebetize(filepath) {
+			fs.readFile(filepath, function(err, data) {
+				if (err) throw err;
+
+				var lines = data.toString().split("\n"),
+					selectors = [],
+					selectorIndex = 0,
+					selectorLine = 0,
+					isSelector = false,
+					cyan = '\u001b[36m';
+
+				_.each(lines, function (line, i) {
+					var selectorStart = /[\{]/g.test(line),
+						selectorEnd = /[\}]/g.test(line);
+
+					grunt.log.writeln('line ' + i + ': ' + line);
+
+					if (!isSelector && selectorStart) {
+						isSelector = true;
+						grunt.log.writeln(cyan + 'selector ' + selectorIndex + ' created' + reset);
+						selectors[selectorIndex] = [];
+					} else if (isSelector && selectorStart) {
+						selectors[selectorIndex].sort();
+						selectorIndex++;
+						selectorLine = 0;
+						selectors[selectorIndex] = [];
+						grunt.log.writeln(cyan + 'nested selector ' + selectorIndex + ' created' + reset);
+					}
+
+					if (isSelector && !selectorStart && !selectorEnd) {
+						grunt.log.writeln(cyan + 'adding property ' + selectorLine + ' to selector ' + selectorIndex + reset);
+						selectors[selectorIndex][selectorLine] = line;
+						selectorLine++;
+					}
+
+					if (isSelector && selectorEnd) {
+						selectors[selectorIndex].sort();
+						isSelector = false;
+						selectorIndex++;
+						selectorLine = 0;
+					}
+				});
+
+				selectorIndex = 0;
+				selectorLine = 0;
+				isSelector = false;
+
+				_.each(lines, function (line, i) {
+					var selectorStart = /[\{]/g.test(line),
+						selectorEnd = /[\}]/g.test(line);
+
+					if (!isSelector && selectorStart) {
+						isSelector = true;
+					} else if (isSelector && selectorStart) {
+						selectorIndex++;
+						selectorLine = 0;
+					}
+
+					if (isSelector && !selectorStart && !selectorEnd) {
+						grunt.log.writeln(cyan + 'line ' + i + ' *: ' + selectors[selectorIndex][selectorLine] + reset);
+						selectorLine++;
+					} else {
+						grunt.log.writeln('line ' + i + ': ' + line);
+					}
+
+					if (isSelector && selectorEnd) {
+						isSelector = false;
+						selectorIndex++;
+						selectorLine = 0;
+					}
+					
+				});
+
+				if (filesComplete ===  filesLength - 1) {
+					done( true );
+				} else {
+					filesComplete++;
+				}
+			});
+		}
+
 	});
 };
