f9663eac3d9dc4c2260f978c677be3815d20c6f5
Richard Jones <rjones@cpbgroup.com>
2013-06-13 11:28:39 -0600

    simplifying code using _.each

--- www/javascripts/app/helpers/console.js
+++ www/javascripts/app/helpers/console.js
@@ -2,7 +2,7 @@
  * @module helpers/console
  */
 
-define(['settings'], function (settings) {
+define(['settings','underscore'], function (settings, _) {
 
 	'use strict';
 
@@ -14,33 +14,28 @@ define(['settings'], function (settings) {
 	var console  = window.console = {};
 	var methods = ['assert', 'count', 'debug', 'dir', 'dirxml', 'error', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'trace', 'warn'];
 	
-	for (var i = methods.length; i--;) {
-		+function (methodName) {
-			if (settings.debug===true) {
-				console[methodName] = function () {
-					if (original && methodName in original) {
-						
-						if (typeof original[methodName]==='function') {
-							original[methodName].apply(original, arguments);
-						} else {
-							// IE<10 does not extend native Object and Function types
-							// therefore .apply does not work so instead we just execute the function
-							// once for each argument
-							for (var i=0; i<arguments.length; i++) {
-								original[methodName](arguments[i]);
-							}
+	_.each(methods, function (methodName) {
+		if (settings.debug===true) {
+			console[methodName] = function () {
+				if (original && methodName in original) {
+					if (typeof original[methodName]==='function') {
+						original[methodName].apply(original, arguments);
+					} else {
+						// IE<10 does not extend native Object and Function types
+						// therefore .apply does not work so instead we just execute the function
+						// once for each argument
+						for (var i=0; i<arguments.length; i++) {
+							original[methodName](arguments[i]);
 						}
 					}
-				};
-			} else {
-				console[methodName] = function () {}; // if debug is not explicitly turned on, do nothing with console methods
-			}
-
-		}(methods[i]);
-	}
+				}
+			};
+		} else {
+			console[methodName] = function () {}; // if debug is not explicitly turned on, do nothing with console methods
+		}
+	});
 
 	return console;
-
 });
 
 
